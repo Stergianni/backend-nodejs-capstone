a0283f538e8dc44bf3f961778b077b2edb7d3897
Stergianni <stella.orfanidis@hotmail.com>
2025-03-18 17:50:43 +0100

    Step 3: Implement the /api/secondchance/items POST endpoint

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -21,9 +21,8 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-// Get all secondChanceItems
-router.get('/', async (req, res, next) => {
-    logger.info('/ called');
+// Add a new secondChanceItem
+router.post('/', upload.single('file'), async (req, res, next) => {
     try {
         // Task 1: Retrieve the database connection
         const db = await connectToDatabase();
@@ -31,13 +30,32 @@ router.get('/', async (req, res, next) => {
         // Task 2: Retrieve the secondChanceItems collection
         const collection = db.collection("secondChanceItems");
 
-        // Task 3: Fetch all secondChanceItems
-        const secondChanceItems = await collection.find({}).toArray();
+        // Task 3: Create a new secondChanceItem from the request body
+        let secondChanceItem = req.body;
 
-        // Task 4: Return the secondChanceItems
-        res.json(secondChanceItems);
+        // Task 4: Get the last id, increment it by 1, and set it to the new secondChanceItem
+        const lastItemQuery = await collection.find().sort({ 'id': -1 }).limit(1).toArray();
+        if (lastItemQuery.length > 0) {
+            secondChanceItem.id = (parseInt(lastItemQuery[0].id) + 1).toString();
+        } else {
+            secondChanceItem.id = "1"; // Default ID if no items exist
+        }
+
+        // Task 5: Set the current date in the new item
+        secondChanceItem.date_added = Math.floor(new Date().getTime() / 1000);
+
+        // Task 6: Add the secondChanceItem to the database
+        const result = await collection.insertOne(secondChanceItem);
+        secondChanceItem._id = result.insertedId;
+
+        // Task 7: Upload its image to the images directory
+        if (req.file) {
+            secondChanceItem.image = `/images/${req.file.filename}`;
+        }
+
+        res.status(201).json(secondChanceItem);
     } catch (e) {
-        logger.error('Oops, something went wrong', e);
+        logger.error('Error adding item:', e);
         next(e);
     }
 });
