f9c92dcf20e473ecaee367ca4a30648295004c4e
Stergianni <stella.orfanidis@hotmail.com>
2025-03-18 18:31:35 +0100

    Step 2: Integrate search routes in app.js

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -21,6 +21,9 @@ app.use(express.json());
 // Items API Task 1: import the secondChanceItemsRoutes and store in a constant called secondChanceItemsRoutes
 const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
 
+// Task 1: Import searchRoutes and store in a constant called searchRoutes
+const searchRoutes = require('./routes/searchRoutes');
+
 // Logger
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -28,6 +31,9 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 app.use('/api/secondchance/items', secondChanceItemsRoutes);
+
+// Task 2: Add the searchRoutes to the server using the app.use() method
+app.use('/api/secondchance/search', searchRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
