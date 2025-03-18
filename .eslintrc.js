a11ee8181b997acd9849a38008a451ec66af2db7
Stergianni <stella.orfanidis@hotmail.com>
2025-03-18 21:19:51 +0100

    CI/CD with GitHub Actions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,26 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true
-  },
-  parserOptions: {
-    ecmaVersion: 'latest'
-  },
-  extends: ['standard'],
-  overrides: [
-    {
-      files: ['.eslintrc.{js,cjs}'],
-      env: {
-        node: true
-      },
-      parserOptions: {
-        sourceType: 'script'
+    env: {
+      browser: true,
+      commonjs: true,
+      es2021: true
+    },
+    extends: 'standard',
+    overrides: [
+      {
+        env: {
+          node: true
+        },
+        files: [
+          '.eslintrc.{js,cjs}'
+        ],
+        parserOptions: {
+          sourceType: 'script'
+        }
       }
+    ],
+    parserOptions: {
+      ecmaVersion: 'latest'
+    },
+    rules: {
     }
-  ],
-  rules: {}
-}
+  }
